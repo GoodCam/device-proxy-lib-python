b35f3edf7a042a82717d9cafcb8051e83d27169e
Ondrej Perutka <perutka.ondrej@gmail.com>
2022-11-24 13:42:58 +0100

    Reuse README in the documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,10 +6,11 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..'))
 project = 'GoodCam Device Proxy'
 copyright = '2022, GoodCam'
 author = 'GoodCam'
-release = '0.1.0'
+release = '0.1.2'
 
 extensions = [
     'sphinx.ext.autodoc',
+    'myst_parser',
 ]
 
 templates_path = ['_templates']
